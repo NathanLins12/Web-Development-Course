ba4ff4ea7e3a10ed294d4c307a6726bcec3ebfdc
Daniel Quintino <danieljucas123@gmail.com>
2023-11-27 16:12:42 -0300

    class 02 arithmetic and comparison

---
+++ 3-javaScript/class-02-operators/1-arithmetic.js
@@ -0,0 +1,43 @@
+let num1 = 81
+let num2 = 3
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 % num2);
+// console.log(num1 ** num2);
+console.log(num1 ** (1 / 2));
+console.log(Math.sqrt (num1));
+
+
+console.log(num2);
+console.log(num2++);
+console.log(num2--);
+console.log(num2);
+console.log(++num2);
+
+
+let num = 2  
+console.log(num);
+console.log(num2--);
+console.log(num);
+console.log(--num2);
+console.log(num);
+console.log(++num2 + 2);
+
+//Assigment
+
+let number;
+
+number = 330
+
+// number = number + 20
+
+number += 20
+number -= 20
+number *= 20
+number /= 20
+number %= 20
+number **= 20
+
+console.log(number);
