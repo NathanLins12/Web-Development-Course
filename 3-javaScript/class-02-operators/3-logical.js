d1af1f9adb3a78764c5a7084aa920de161c8f684
Daniel Quintino <danieljucas123@gmail.com>
2023-12-07 15:25:06 -0300

    class 02 operators

---
+++ 3-javaScript/class-02-operators/3-logical.js
@@ -0,0 +1,12 @@
+// Logical Operators (Boolean)
+
+// and  &&
+// or   ||
+// not  !
+
+
+let credCard = true;
+let hasCredit = true;
+
+console.log(!!credCard);
+console.log(hasCredit);
---
+++ 3-javaScript/class-02-operators/4-types.js
@@ -0,0 +1,19 @@
+// Binary
+
+let num1 = 2
+let num2 = 3
+console.log(num1 / num2);
+
+// Unary
+
+console.log(++num1);
+console.log(typeof num2);
+console.log(typeof + "10");
+
+// ternary
+// test ? true : false
+
+console.log(5 < 3 ? "sim" : "naao");
+
+let average = 7
+console.log(average >= 7 ? "aprovado" : "reprovado");
