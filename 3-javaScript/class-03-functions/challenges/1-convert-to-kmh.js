1df167310127c51554eb72822380f01ca6c09574
Daniel Quintino <danieljucas123@gmail.com>
2023-12-11 15:34:26 -0300

    add challenges of the class 03

---
+++ 3-javaScript/class-03-functions/challenges/1-convert-to-kmh.js
@@ -0,0 +1,14 @@
+// Declare uma função para converter velocidade de m/s para km/h
+// Solicite a velocidade via prompt e passe como argumento
+// Execute a função e escreva o resultado
+
+function convertToKmh(speedMs) {
+    return speedMs * 3.6;
+  }
+  
+  const userSpeedMs = Number(prompt("Digite a velocidade em M/s:"));
+  
+  const convertedSpeed = convertToKmh(userSpeedMs);
+  
+  window.alert(`${userSpeedMs}m/s é ${convertedSpeed}km/h`);
+  
---
+++ 3-javaScript/class-03-functions/challenges/2-imc-calculator.js
@@ -0,0 +1,11 @@
+// Crie uma função que calcule o IMC do usuário
+// A função deve receber os parâmetros de height e de weight;
+// Escreva na tela o IMC e se está bom ou rium seguindo a tabela
+
+function calcIMC(height, weight) {
+    const imc = weight / (height / 100) ** 2;
+    return imc.toFixed(2);
+  }
+  
+  console.log(calcIMC(185, 80));
+  
